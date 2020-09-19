47edca9bffbe40ac82dd7323648880ef597fb74a
Douglas <douglasbelinato@cvccorp.com.br>
2020-09-19 14:44:55 -0300

    Criação do projeto e configuração de conexão ao Mysql; Criação do docker-compose com Mysql e Adminer

---
+++ src/test/java/com/accounting/accountingapi/AccountingApiApplicationTests.java
@@ -0,0 +1,11 @@
+package com.accounting.accountingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest class AccountingApiApplicationTests {
+
+    @Test void contextLoads() {
+    }
+
+}
