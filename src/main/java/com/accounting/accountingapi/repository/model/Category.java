38320e42b7c2c2c365667597986db25174ab7954
Douglas <douglasbelinato@cvccorp.com.br>
2020-10-03 12:45:50 -0300

    Adding lombok to Category entity

--- src/main/java/com/accounting/accountingapi/repository/model/Category.java
+++ src/main/java/com/accounting/accountingapi/repository/model/Category.java
@@ -1,4 +1,6 @@
-package com.accounting.accountingapi.model;
+package com.accounting.accountingapi.repository.model;
+
+import lombok.*;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
@@ -7,31 +9,20 @@ import java.util.Objects;
 
 @Entity
 @Table(name = "category")
+@Builder
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Category {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
     private Long id;
 
     @NotBlank
     @Size(min = 3, max = 50)
     private String name;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 
     @Override
     public boolean equals(Object o) {
