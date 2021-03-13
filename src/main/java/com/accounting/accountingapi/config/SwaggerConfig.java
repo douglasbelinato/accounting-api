8b2a26c89f152745c372850dd88eb651ff1b18d5
Douglas Belinato <douglas_belinato@hotmail.com>
2021-03-13 18:47:06 -0300

    Adding API documentation with Open API.

---
+++ src/main/java/com/accounting/accountingapi/config/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package com.accounting.accountingapi.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Accounting API",
+                description = "" +
+                        "API for accounting operations",
+                contact = @Contact(
+                        name = "Douglas Belinato",
+                        url = "https://github.com/douglasbelinato/",
+                        email = "douglas_belinato@hotmail.com"
+                ),
+                license = @License(
+                        name = "MIT Licence",
+                        url = "https://github.com/douglasbelinato/accounting-api/blob/master/LICENSE")),
+        servers = @Server(url = "http://localhost:8080")
+)
+public class SwaggerConfig {
+}
--- src/main/java/com/accounting/accountingapi/resource/PersonResource.java
+++ src/main/java/com/accounting/accountingapi/resource/PersonResource.java
@@ -1,12 +1,21 @@
 package com.accounting.accountingapi.resource;
 
 import com.accounting.accountingapi.event.ResourceCreatedEvent;
+import com.accounting.accountingapi.exception.dto.ErrorResponseDTO;
 import com.accounting.accountingapi.mapper.PersonMapper;
 import com.accounting.accountingapi.resource.converter.ResourcePartialUpdateConverter;
 import com.accounting.accountingapi.resource.dto.PersonDTO;
 import com.accounting.accountingapi.resource.dto.PersonPartialUpdateDTO;
 import com.accounting.accountingapi.service.PersonService;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
@@ -20,6 +29,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/people")
+@Tag(name = "Person", description = "Operations for person")
 public class PersonResource {
 
     @Autowired
@@ -34,17 +44,40 @@ public class PersonResource {
     @Autowired
     private ResourcePartialUpdateConverter<PersonDTO, PersonPartialUpdateDTO> personPartialUpdateConverter;
 
+
+    @Operation(summary = "List people", description = "List all people saved in our database")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Search was successful",
+                    content = @Content(array = @ArraySchema(schema = @Schema(implementation = PersonDTO.class))))
+    })
     @GetMapping
     public ResponseEntity<List<PersonDTO>> findAll() {
         return ResponseEntity.ok().body(personMapper.fromPersonModelListToPersonDtoList(personService.findAll()));
     }
 
+
+    @Operation(summary = "Find by id", description = "Find a person by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Search was successful",
+                    content = @Content(array = @ArraySchema(schema = @Schema(implementation = PersonDTO.class)))),
+            @ApiResponse(responseCode = "404", description = "Person not found",
+                    content = @Content(schema = @Schema(hidden = true)))
+    })
     @GetMapping("/{id}")
-    public ResponseEntity<PersonDTO> findById(@PathVariable("id") Long id) {
+    public ResponseEntity<PersonDTO> findById(@Parameter(description = "Person id")
+                                                  @PathVariable("id") Long id) {
         var person = personService.findById(id);
         return ResponseEntity.ok().body(personMapper.fromPersonModelToPersonDto(person));
     }
 
+
+    @Operation(summary = "Save person", description = "Save a new person")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "New person successfully saved",
+                    content = @Content(schema = @Schema(implementation = PersonDTO.class))),
+            @ApiResponse(responseCode = "400", description = "Error - Please verify response description for more details",
+                    content = @Content(schema = @Schema(implementation = ErrorResponseDTO.class)))
+    })
     @PostMapping
     public ResponseEntity<PersonDTO> save(@Valid @RequestBody PersonDTO personDTO, HttpServletResponse response) {
         var personSaved = personMapper.fromPersonModelToPersonDto(personService.save(personMapper.fromPersonDtoToPersonModel(personDTO)));
@@ -54,12 +87,28 @@ public class PersonResource {
         return ResponseEntity.status(HttpStatus.CREATED).body(personSaved);
     }
 
+
+    @Operation(summary = "Delete person", description = "Delete a person")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Person successfully deleted",
+                    content = @Content(schema = @Schema(hidden = true))),
+            @ApiResponse(responseCode = "400", description = "Error - Please verify response description for more details",
+                    content = @Content(schema = @Schema(implementation = ErrorResponseDTO.class)))
+    })
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable Long id) {
          personService.delete(id);
     }
 
+
+    @Operation(summary = "Update person partially", description = "Update specific fields of a person")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Person successfully updated",
+                    content = @Content(schema = @Schema(hidden = true))),
+            @ApiResponse(responseCode = "400", description = "Error - Please verify response description for more details",
+                    content = @Content(schema = @Schema(implementation = ErrorResponseDTO.class)))
+    })
     @PatchMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void patch(@PathVariable Long id, @RequestBody Map<String, Object> jsonMapResourceUpdated) throws JsonProcessingException {
@@ -68,5 +117,6 @@ public class PersonResource {
 
         personService.update(personMapper.fromPersonDtoToPersonModel(personUpdatedDTO));
     }
+
 
 }
